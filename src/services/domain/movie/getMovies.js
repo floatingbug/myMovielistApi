f2dbf6f839b49a22ea3c9c3cde458120116dacb8
floatingbug <sieber.thomas@yahoo.com>
2025-05-14 14:54:59 +0200

    new

--- src/services/domain/movie/getMovies.js
+++ src/services/domain/movie/getMovies.js
@@ -1,15 +1,34 @@
 const tmdb = require("@tmdb");
+const ratingModel = require("@models/rating");
 
 
 async function getMovies({query}){
 	try{
-		const result = await tmdb.fetchMovies({query});
+		const fetchedMovies = await tmdb.fetchMovies({query});
+
+		// get additional informations from db
+		const movieIds = fetchedMovies.results.map(movie => movie.id);
+		
+		// get ratings
+		const ratingQuery = {
+			movieId: {$in: movieIds},
+		}
+		const ratings = await ratingModel.getRatings({query: ratingQuery});
+
+		// add the additional information to movies
+		ratings?.forEach(rating => {
+			fetchedMovies.results?.forEach(movie => {
+				if(rating.movieId === movie.movieId){
+					movie.rating = rating;
+				}
+			});
+		});
 
 		return {
 			success: true,
 			code: 200,
 			message: "Sent movies.",
-			data: result,
+			data: fetchedMovies,
 		};
 	}
 	catch(error){
